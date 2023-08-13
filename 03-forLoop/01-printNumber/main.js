cce07fab165242300499af80f5d79f3198931456
Nitradee8 <nbtt.nik@gmail.com>
2023-08-14 00:42:07 +0700

    updatePN

---
+++ 03-forLoop/01-printNumber/main.js
@@ -0,0 +1,15 @@
+// ให้เขียนโค้ดเพื่อแสดงเลขคู่ตั้งแต่ 1 - 100 
+
+// let i = 0
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+// console.log('end...')
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+console.log('end...');
