2528679773ee6c4f5e9bf93c15517e6118ffa616
Nitradee8 <nbtt.nik@gmail.com>
2023-08-14 00:59:44 +0700

    demo

---
+++ 01-while/_demo/main.js
@@ -0,0 +1,91 @@
+console.log('lean counting loop');
+
+console.log('starting......');
+
+// () = parenthesis = วงเล็บ
+// {} = bracket = ปีกกา
+// [] = square bracket = ก้ามปู
+
+
+// for (let i = 0; i < 3; i++) {
+//     console.log('>> Hello');
+//     console.log('>>>Codecamp')
+// }
+
+// console.log('end...');
+// ----------------------------------------------
+
+// Ex1 : Exmine Syntax
+// for (let i = 2; i <= 20; i = i + 2) {
+//     // if (i % 2 == 0) console.log('i');
+//     console.log(i);
+// }
+
+// console.log('end...');
+// ----------------------------------------------
+
+// Ex2 : fizzBuzz
+    // for (let i = 1; i <= 20; i = i + 1) {
+    //     // console.log(i);
+    //     if (i % 3 == 0 && i $ 5 == 0) console.log("Buzz")
+    //     else if (i % 5 == 0 && i $ 5 == 0) console.log("Buzz")
+    // }
+    // console.log('end...')
+// ----------------------------------------------
+
+// Ex3 : Skip Fizz
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 == 0 && 1 % 5 == 0 ) console.log('FizzBuzz');
+//     else if (i % 5 == 0) console.log('Buzz');
+//     else if (i % 3 == 0) continue;
+//     else ('');
+// }
+// console.log('end...')
+// ----------------------------------------------
+
+// Ex3B :
+
+
+
+
+
+
+
+
+// Ex4 : Concat String
+    
+    // let str = '';
+
+    // for (let i = 1; i <= 10; i++) {
+
+    //     if(i % 3 == 0) continue;
+    //     str += i;
+    
+    // }
+
+    // console.log(str); //12345678910
+    // console.log('end...')
+// ----------------------------------------------
+
+// Ex5 : remove vowel (a, e, i, o, u)
+
+    // let src = "codecamp";
+    // let rec = ""; //cdcmp
+    
+    // //
+    // //
+    // //
+
+    // for (let i = 0; i < src.length; i++) {
+            // console.log(i, src[i]);
+    //     let c = src[i];
+    //     if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') continue;
+    //     res = res + src[i];
+    //     // '' + src[0] == '' + 'c' => 'c'
+    //     // 'c' + src[i] == 'c' + 'o' => 'co'
+    // }
+
+    // console.log(res);
+    // console.log('end...')
+
+
