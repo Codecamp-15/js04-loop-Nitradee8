bfce84809c98496c9fa0dec7347b8feb185649e8
Nitradee8 <nbtt.nik@gmail.com>
2023-08-14 15:56:09 +0700

    loop

---
+++ 01-while/03-printEven/main.js
@@ -0,0 +1,11 @@
+// ให้เขียนโค้ดเพื่อแสดงเลขคู่ตั้งแต่ 1 - 100 
+
+// let i = 0;
+//     while (i++ < 100) alert(i);
+
+let i = 0;
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+console.log('end...')
